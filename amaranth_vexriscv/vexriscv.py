69952405c4147758bd20175eec2a36a9afe41c2a
Jean-François Nguyen <jf@jfng.fr>
2024-02-20 19:04:44 +0100

    Update to use amaranth.lib.wiring and minor cleanups.

--- amaranth_vexriscv/vexriscv.py
+++ amaranth_vexriscv/vexriscv.py
@@ -1,89 +1,119 @@
+import enum
+from pathlib import Path
+
 from amaranth import *
-from amaranth.utils import log2_int
+from amaranth.lib import wiring
+from amaranth.lib.wiring import In, Out
+from amaranth.utils import bits_for
 
 from amaranth_soc import wishbone
 
-from pathlib import Path
+
+__all__ = ["VexRiscvConfig", "VexRiscv"]
 
 
-class VexRiscv(Elaboratable):
-    def __init__(self, config="LiteDebug", reset_vector=0x00100000):
-        self.config = config
-        self.dbus = wishbone.Interface(addr_width=30,
-                                      data_width=32, granularity=8, features={"cti", "bte"}) 
-        self.ibus = wishbone.Interface(addr_width=30,
-                                      data_width=32, granularity=8, features={"cti", "bte"})
-        self.timer_irq = Signal()
-        self.software_irq = Signal()
-        self.irq_sigs = [None for i in range(32)]
-        self.debug_rst_out = Signal()
-        self.debug_rst_in = None
-        self.jtag_tms = Signal()
-        self.jtag_tdi = Signal()
-        self.jtag_tdo = Signal()
-        self.jtag_tck = Signal()
-        self.reset_vector = reset_vector
+class VexRiscvConfig(enum.Enum):
+    LITE            = "Lite"
+    LITE_DEBUG      = "LiteDebug"
+    LINUX_MPW_5     = "LinuxMPW5"
+    LINUX_MPW_5L    = "LinuxMPW5L"
+    LINUX_MPW_CACHE = "LinuxMPWCache"
 
-    def add_irq(self, irq, sig):
-        assert self.irq_sigs[irq] is None
-        self.irq_sigs[irq] = sig
+    def with_debug_plugin(self):
+        return self != self.LITE
+
+
+class VexRiscv(wiring.Component):
+    def __init__(self, *, config="LiteDebug", reset_vector=0x00100000):
+        if not isinstance(reset_vector, int) or reset_vector < 0:
+            raise TypeError(f"Reset vector address must be a non-negative integer, not "
+                            f"{reset_vector!r}")
+        if bits_for(reset_vector) > 32:
+            raise ValueError(f"Reset vector address {reset_vector:#x} cannot be represented as a "
+                             f"32-bit integer")
+
+        self._config       = VexRiscvConfig(config)
+        self._reset_vector = reset_vector
+
+        super().__init__({
+            "ibus": Out(wishbone.Signature(addr_width=30, data_width=32, granularity=8,
+                                           features=("err", "cti", "bte"))),
+            "dbus": Out(wishbone.Signature(addr_width=30, data_width=32, granularity=8,
+                                           features=("err", "cti", "bte"))),
+
+            "timer_irq":     In(unsigned(1)),
+            "software_irq":  In(unsigned(1)),
+            "external_irqs": In(unsigned(32)),
+
+            "debug_rst_in":  In(unsigned(1)),
+            "debug_rst_out": Out(unsigned(1)),
+
+            "jtag_tms": In(unsigned(1)),
+            "jtag_tdi": In(unsigned(1)),
+            "jtag_tdo": Out(unsigned(1)),
+            "jtag_tck": In(unsigned(1)),
+        })
+
+    @property
+    def config(self):
+        return self._config
+
+    @property
+    def reset_vector(self):
+        return self._reset_vector
 
     def elaborate(self, platform):
         m = Module()
-        ext_irq = Signal(32)
-        for i, irq in enumerate(self.irq_sigs):
-            if irq is not None:
-                m.d.comb += ext_irq[i].eq(irq)
-            else:
-                m.d.comb += ext_irq[i].eq(0)
-        conn = dict(
-            i_timerInterrupt=self.timer_irq,
-            i_softwareInterrupt=self.software_irq,
-            i_externalInterruptArray=ext_irq,
-            o_debug_resetOut=self.debug_rst_out,
 
-            o_iBusWishbone_CYC=self.ibus.cyc,
-            o_iBusWishbone_STB=self.ibus.stb,
-            i_iBusWishbone_ACK=self.ibus.ack,
-            o_iBusWishbone_WE=self.ibus.we,
-            o_iBusWishbone_ADR=self.ibus.adr,
-            i_iBusWishbone_DAT_MISO=self.ibus.dat_r,
-            o_iBusWishbone_DAT_MOSI=self.ibus.dat_w,
-            o_iBusWishbone_SEL=self.ibus.sel,
-            i_iBusWishbone_ERR=0,
-            o_iBusWishbone_CTI=self.ibus.cti,
-            o_iBusWishbone_BTE=self.ibus.bte,
+        instance_kwargs = {
+            "i_clk": ClockSignal(),
+            "i_reset": ResetSignal(),
+            "i_externalResetVector": Const(self.reset_vector, unsigned(32)),
 
-            o_dBusWishbone_CYC=self.dbus.cyc,
-            o_dBusWishbone_STB=self.dbus.stb,
-            i_dBusWishbone_ACK=self.dbus.ack,
-            o_dBusWishbone_WE=self.dbus.we,
-            o_dBusWishbone_ADR=self.dbus.adr,
-            i_dBusWishbone_DAT_MISO=self.dbus.dat_r,
-            o_dBusWishbone_DAT_MOSI=self.dbus.dat_w,
-            o_dBusWishbone_SEL=self.dbus.sel,
-            i_dBusWishbone_ERR=0,
-            o_dBusWishbone_CTI=self.dbus.cti,
-            o_dBusWishbone_BTE=self.dbus.bte,
+            "o_iBusWishbone_CYC": self.ibus.cyc,
+            "o_iBusWishbone_STB": self.ibus.stb,
+            "i_iBusWishbone_ACK": self.ibus.ack,
+            "o_iBusWishbone_WE": self.ibus.we,
+            "o_iBusWishbone_ADR": self.ibus.adr,
+            "i_iBusWishbone_DAT_MISO": self.ibus.dat_r,
+            "o_iBusWishbone_DAT_MOSI": self.ibus.dat_w,
+            "o_iBusWishbone_SEL": self.ibus.sel,
+            "i_iBusWishbone_ERR": self.ibus.err,
+            "o_iBusWishbone_CTI": self.ibus.cti,
+            "o_iBusWishbone_BTE": self.ibus.bte,
 
-            i_clk=ClockSignal(),
-            i_reset=ResetSignal(),
-            i_externalResetVector=self.reset_vector,
-        )
-        if "Debug" in self.config or "LinuxMPW" in self.config:
-            # Variants with JTAG
-            conn["i_jtag_tms"] = self.jtag_tms
-            conn["i_jtag_tdi"] = self.jtag_tdi
-            conn["o_jtag_tdo"] = self.jtag_tdo
-            conn["i_jtag_tck"] = self.jtag_tck
-            conn["i_debugReset"] = ResetSignal() if self.debug_rst_in is None else self.debug_rst_in
-            conn["o_debug_resetOut"] = self.debug_rst_out
+            "o_dBusWishbone_CYC": self.dbus.cyc,
+            "o_dBusWishbone_STB": self.dbus.stb,
+            "i_dBusWishbone_ACK": self.dbus.ack,
+            "o_dBusWishbone_WE": self.dbus.we,
+            "o_dBusWishbone_ADR": self.dbus.adr,
+            "i_dBusWishbone_DAT_MISO": self.dbus.dat_r,
+            "o_dBusWishbone_DAT_MOSI": self.dbus.dat_w,
+            "o_dBusWishbone_SEL": self.dbus.sel,
+            "i_dBusWishbone_ERR": self.dbus.err,
+            "o_dBusWishbone_CTI": self.dbus.cti,
+            "o_dBusWishbone_BTE": self.dbus.bte,
 
-        m.submodules.vex = Instance(
-            "VexRiscv",
-            **conn
-        )
-        path = Path(__file__).parent / f"verilog/VexRiscv_{self.config}.v"
+            "i_timerInterrupt": self.timer_irq,
+            "i_softwareInterrupt": self.software_irq,
+            "i_externalInterruptArray": self.external_irqs,
+        }
+
+        if self.config.with_debug_plugin():
+            instance_kwargs.update({
+                "i_debugReset": self.debug_rst_in,
+                "o_debug_resetOut": self.debug_rst_out,
+
+                "i_jtag_tms": self.jtag_tms,
+                "i_jtag_tdi": self.jtag_tdi,
+                "o_jtag_tdo": self.jtag_tdo,
+                "i_jtag_tck": self.jtag_tck,
+            })
+
+        m.submodules.vexriscv = Instance("VexRiscv", **instance_kwargs)
+
+        path = Path(__file__).parent / f"verilog/VexRiscv_{self.config.value}.v"
         with open(path, 'r') as f:
             platform.add_file(path.name, f)
+
         return m
